435e23308dfbad4317abc56764290f17d677632d
Camen <camenpihor@gmail.com>
2018-12-21 00:04:22 -0500

    Delete comments from router

--- src/routes/_router.js
+++ src/routes/_router.js
@@ -1,4 +1,3 @@
-/* eslint-disable implicit-arrow-linebreak */
 import Vue from 'vue'
 import Router from 'vue-router'
 import store from '@/store.js'
@@ -17,31 +16,27 @@ const router = new Router({
     },
     {
       path: '/questions',
-      name: 'questions',
       meta: { coreObject: 'questions' },
       component: () =>
-        import(/* webpackChunkName: "questions" */ './question/Home.vue'),
+        import('./question/Home.vue'),
     },
     {
       path: '/questions/archive',
-      name: 'questions-archive',
       meta: { coreObject: 'questions' },
       component: () =>
-        import(/* webpackChunkName: "questions-archive" */ './question/Archive.vue'),
+        import('./question/Archive.vue'),
     },
     {
       path: '/questions/stats',
-      name: 'questions-stats',
       meta: { coreObject: 'questions' },
       component: () =>
-        import(/* webpackChunkName: "questions-stats" */ './question/Stats.vue'),
+        import('./question/Stats.vue'),
     },
     {
       path: '/questions/:id',
-      name: 'questions-detail',
       meta: { coreObject: 'questions' },
       component: () =>
-        import(/* webpackChunkName: "questions-describe" */ './question/Detail.vue'),
+        import('./question/Detail.vue'),
       beforeEnter: (to, from, next) => {
         coreObjectDetail("question", to.params.id)
           .get()
@@ -52,7 +47,7 @@ const router = new Router({
     {
       path: '/404',
       component: () =>
-        import(/* webpackChunkName: "404" */ './NotFound.vue'),
+        import('./NotFound.vue'),
     },
     {
       path: '*',
