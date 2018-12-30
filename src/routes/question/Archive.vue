d2381a37832f24651bf67d303c7ca9ad2ff70de3
Camen <camenpihor@gmail.com>
2018-12-30 17:37:10 -0600

    use cards for questions

--- src/routes/question/Archive.vue
+++ src/routes/question/Archive.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="question-archive">
-    <hello-world msg="Welcome to Question Archive"/>
-    <question-list v-bind:questions="questions"/>
+    <h1>Archive</h1>
+    <sui-card-group :items-per-row="1" id="question-list">
+      <sui-card v-for="question in questions" :key="question.id">
+        <sui-card-content>
+          <sui-card-header>
+            <router-link :to="'/questions/' + question.id">{{ question.question }}</router-link>
+          </sui-card-header>
+          <sui-card-meta>{{ question.created_at_utc | moment("dddd, MMMM Do YYYY") }}</sui-card-meta>
+          <sui-card-description>{{ question.num_views }} | {{ question.rating }}</sui-card-description>
+        </sui-card-content>
+      </sui-card>
+    </sui-card-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-import QuestionList from "@/components/QuestionList.vue";
 import { coreObjectList } from "@/api.js";
 
 export default {
   name: "question-archive",
-  components: {
-    HelloWorld,
-    QuestionList
-  },
+  components: {},
   data() {
     return {
       questions: null,
@@ -45,5 +50,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#question-list {
+  max-width: 600px;
+  margin: auto;
 }
 </style>
--- src/routes/question/Home.vue
+++ src/routes/question/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="question-home">
-    <hello-world msg="Welcome to Questions"/>
+    <h1>Welcome to Questions</h1>
     <sui-card-group :items-per-row="1" id="question-list">
       <sui-card v-for="question in questions" :key="question.id">
         <sui-card-content>
@@ -10,30 +10,17 @@
           <sui-card-meta>{{ question.created_at_utc | moment("dddd, MMMM Do YYYY") }}</sui-card-meta>
           <sui-card-description>{{ question.num_views }} | {{ question.rating }}</sui-card-description>
         </sui-card-content>
-        <!-- <sui-card-content extra>
-          <sui-container text-align="center">
-            <sui-button-group>
-              <sui-button>Edit</sui-button>
-              <sui-button-or/>
-              <sui-button positive>View</sui-button>
-            </sui-button-group>
-          </sui-container>
-        </sui-card-content> -->
       </sui-card>
     </sui-card-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-import QuestionList from "@/components/QuestionList.vue";
 import { coreObjectList, getRandomSubset } from "@/api.js";
 
 export default {
   name: "question-home",
-  components: {
-    HelloWorld
-  },
+  components: {},
   data() {
     return {
       questions: null,
@@ -66,7 +53,7 @@ export default {
 }
 
 #question-list {
-    max-width: 600px;
-    margin: auto
+  max-width: 600px;
+  margin: auto;
 }
 </style>
