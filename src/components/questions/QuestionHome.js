c51d4c446cd57ff1641a6f2ead23f32448100ce6
Camen <camenpihor@gmail.com>
2019-02-01 20:07:25 -0500

    Mobile on tap to edit notebook

--- src/components/questions/QuestionHome.js
+++ src/components/questions/QuestionHome.js
@@ -2,12 +2,218 @@ import React, { Component } from 'react'
 import moment from 'moment'
 import Moment from 'react-moment'
 import { NavLink } from 'react-router-dom'
-import { Button, Card, Form, Grid, Icon, Message } from 'semantic-ui-react'
+import { Button, Card, Form, Grid, Icon, List, Message } from 'semantic-ui-react'
 import Markdown from 'react-markdown'
 import AppNavigation from 'components/Navigation'
 import { coreObjectList, coreObjectNotebook, getRandomSubset } from 'api'
 import 'style/questions.css';
 
+
+function QuestionStats() {
+  return (
+    <List animated className="home-section">
+      <List.Item>
+        Last edit:
+    </List.Item>
+      <List.Item>
+        Longest streak:
+    </List.Item>
+      <List.Item>
+        Edits per month:
+    </List.Item>
+      <List.Item>
+        Number of questions:
+    </List.Item>
+      <List.Item>
+        Number of questions:
+    </List.Item>
+    </List>
+  )
+}
+
+class QuestionList extends Component {
+  componentDidMount() {
+    coreObjectList("question")
+      .get()
+      .then(response => {
+        this.setState({
+          questions: getRandomSubset(response.data, 5)
+        });
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          this.props.history.push("/")
+          sessionStorage.setItem("token", null)
+        } else {
+          console.log(error)
+        }
+      })
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+    };
+  }
+
+  render() {
+    const { questions } = this.state;
+
+    return (
+      <div className="home-section" >
+        <Grid columns={2} divided className="question-grid">
+          {questions.map(question => (
+            <Grid.Row key={question.id} as={NavLink} to={`/questions/${question.id}`}>
+              <Grid.Column className="column date">
+                <Moment format="MMM DD YYYY">
+                  {question.created_at_utc}
+                </Moment>
+              </Grid.Column>
+              <Grid.Column className="column text">
+                {question.question}
+              </Grid.Column>
+            </Grid.Row>
+          ))}
+        </Grid>
+      </div>
+    )
+  }
+}
+
+function QuestionLinks() {
+  const links = [
+    { title: "SlateStarCodex", url: "https://slatestarcodex.com" },
+    { title: "LessWrong", url: "https://lesswrong.com" },
+    { title: "The Zvi", url: "https://thezvi.wordpress.com" },
+    { title: "Overcoming Bias", url: "http://www.overcomingbias.com" },
+    { title: "Melting Asphalt", url: "http://meltingasphalt.com" }
+  ]
+
+  return (
+    <div className="home-section" >
+      <Card.Group itemsPerRow={2}>
+        {links.map(link => (
+          <Card
+            href={link.url}
+            header={link.title}
+            rel="noopener noreferrer"
+            target="_blank"
+            key={link.title}
+            meta={link.url}
+            className="link"
+            fluid
+          />
+        ))}
+      </Card.Group>
+    </div>
+  )
+}
+
+class QuestionForm extends Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleShowQuestionForm, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleShowQuestionForm, false);
+  }
+
+  constructor(props) {
+    super(props);
+    this.questionForm = React.createRef();
+    this.state = {
+      visible: false,
+      error: null,
+      success: null,
+      errorMessage: null,
+      questionID: null,
+      question: null
+    };
+  }
+
+  handleShowQuestionForm = (event) => {
+    if (event.ctrlKey && event.key === 'n') {
+      window.removeEventListener("keydown", this.handleShowQuestionForm, false);
+      this.showQuestionForm()
+    }
+  }
+
+  showQuestionForm = () => {
+    this.setState({
+      visible: true
+    }, () => {
+      window.scrollTo({
+        top: this.questionForm.current.offsetTop,
+        behavior: "smooth"
+      });
+    })
+  }
+
+  handleQuestionFormSubmit = (event) => {
+    const data = new FormData(event.target)
+    coreObjectList("question")
+      .post(data)
+      .then(response => {
+        this.setState({
+          success: true,
+          questionID: response.data.id,
+          question: response.data.question
+        }, () => {
+          document.getElementById("question-form").reset();
+        });
+        setTimeout(function () {
+          this.setState({ success: null });
+        }.bind(this), 5000);
+      })
+      .catch(error => {
+        this.setState({
+          error: true,
+          errorMessage: error
+        });
+        setTimeout(function () {
+          this.setState({ error: null, errorMessage: null });
+        }.bind(this), 5000);
+      })
+  }
+
+  render() {
+    const { visible, error, success, errorMessage, question, questionID } = this.state;
+
+    return (
+      <div className="home-section">
+        {!visible &&
+          < Button icon circular size="huge" className="create-button" onClick={this.showQuestionForm} >
+            <Icon name='add' />
+          </Button >
+        }
+        {visible &&
+          <div ref={this.questionForm}>
+            <p className="question-header">Create</p>
+            <Form
+              onSubmit={this.handleQuestionFormSubmit}
+              error={error}
+              success={success}
+              id="question-form"
+            >
+              <Form.Input placeholder='Rating' type='number' max={100} name="rating" />
+              <Form.TextArea placeholder='Question' name="question" autoFocus />
+              <Message
+                success
+                header='Question Created'
+                as={NavLink}
+                to={`/questions/${questionID}`}
+                content={question}
+              />
+              <Message error header='Form Error' content={errorMessage} />
+              <Button type='submit'>Submit</Button>
+            </Form>
+          </div>
+        }
+      </div>
+    );
+  }
+}
 
 class Notebook extends Component {
   componentDidMount() {
@@ -191,7 +397,7 @@ class Notebook extends Component {
           </Form>
         }
         {showMarkdown &&
-          <div onMouseDown={this.handleMarkdownClick}>
+          <div onMouseDown={this.handleMarkdownClick} onTouchStartCapture={this.editText}>
             <Markdown
               source={sourceText}
               linkTarget="_blank"
@@ -203,203 +409,6 @@ class Notebook extends Component {
   }
 }
 
-class QuestionForm extends Component {
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleShowQuestionForm, false);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleShowQuestionForm, false);
-  }
-
-  constructor(props) {
-    super(props);
-    this.questionForm = React.createRef();
-    this.state = {
-      visible: false,
-      error: null,
-      success: null,
-      errorMessage: null,
-      questionID: null,
-      question: null
-    };
-  }
-
-  handleShowQuestionForm = (event) => {
-    if (event.ctrlKey && event.key === 'n') {
-      window.removeEventListener("keydown", this.handleShowQuestionForm, false);
-      this.showQuestionForm()
-    }
-  }
-
-  showQuestionForm = () => {
-    this.setState({
-      visible: true
-    }, () => {
-      window.scrollTo({
-        top: this.questionForm.current.offsetTop,
-        behavior: "smooth"
-      });
-    })
-  }
-
-  handleQuestionFormSubmit = (event) => {
-    const data = new FormData(event.target)
-    coreObjectList("question")
-      .post(data)
-      .then(response => {
-        this.setState({
-          success: true,
-          questionID: response.data.id,
-          question: response.data.question
-        }, () => {
-          document.getElementById("question-form").reset();
-        });
-        setTimeout(function () {
-          this.setState({ success: null });
-        }.bind(this), 5000);
-      })
-      .catch(error => {
-        this.setState({
-          error: true,
-          errorMessage: error
-        });
-        setTimeout(function () {
-          this.setState({ error: null, errorMessage: null });
-        }.bind(this), 5000);
-      })
-  }
-
-  render() {
-    const { visible, error, success, errorMessage, question, questionID } = this.state;
-
-    return (
-      <div className="home-section">
-        {!visible &&
-          < Button icon circular size="huge" className="create-button" onClick={this.showQuestionForm} >
-            <Icon name='add' />
-          </Button >
-        }
-        {visible &&
-          <div ref={this.questionForm}>
-            <p className="question-header">Create</p>
-            <Form
-              onSubmit={this.handleQuestionFormSubmit}
-              error={error}
-              success={success}
-              id="question-form"
-            >
-              <Form.Input placeholder='Rating' type='number' max={100} name="rating" />
-              <Form.TextArea placeholder='Question' name="question" autoFocus />
-              <Message
-                success
-                header='Question Created'
-                as={NavLink}
-                to={`/questions/${questionID}`}
-                content={question}
-              />
-              <Message error header='Form Error' content={errorMessage} />
-              <Button type='submit'>Submit</Button>
-            </Form>
-          </div>
-        }
-      </div>
-    );
-  }
-}
-
-function QuestionStats() {
-  return (
-    <div className="home-section">
-      <ul className="question-list">
-        <li className="question-list-item">Last edit:</li>
-        <li className="question-list-item">Longest streak:</li>
-        <li className="question-list-item">Edits per month:</li>
-        <li className="question-list-item">Number of questions:</li>
-        <li className="question-list-item">Number of forgotten questions:</li>
-      </ul>
-    </div>
-  )
-}
-
-class QuestionList extends Component {
-  componentDidMount() {
-    coreObjectList("question")
-      .get()
-      .then(response => {
-        this.setState({
-          questions: getRandomSubset(response.data, 5)
-        });
-      })
-      .catch(error => {
-        if (error.response.status === 401) {
-          this.props.history.push("/")
-          sessionStorage.setItem("token", null)
-        } else {
-          console.log(error)
-        }
-      })
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions: [],
-    };
-  }
-
-  render() {
-    const { questions } = this.state;
-
-    return (
-      <div className="home-section" >
-        <Grid columns={2} divided className="question-grid">
-          {questions.map(question => (
-            <Grid.Row key={question.id} as={NavLink} to={`/questions/${question.id}`}>
-              <Grid.Column className="column date">
-                <Moment format="MMM DD YYYY">
-                  {question.created_at_utc}
-                </Moment>
-              </Grid.Column>
-              <Grid.Column className="column text">
-                {question.question}
-              </Grid.Column>
-            </Grid.Row>
-          ))}
-        </Grid>
-      </div>
-    )
-  }
-}
-
-function QuestionLinks() {
-  const links = [
-    { title: "SlateStarCodex", url: "https://slatestarcodex.com" },
-    { title: "LessWrong", url: "https://lesswrong.com" },
-    { title: "The Zvi", url: "https://thezvi.wordpress.com" },
-    { title: "Overcoming Bias", url: "http://www.overcomingbias.com" },
-    { title: "Melting Asphalt", url: "http://meltingasphalt.com" }
-  ]
-
-  return (
-    <div className="home-section" >
-      <Card.Group itemsPerRow={2}>
-        {links.map(link => (
-          <Card
-            href={link.url}
-            header={link.title}
-            rel="noopener noreferrer"
-            target="_blank"
-            key={link.title}
-            meta={link.url}
-          />
-        ))}
-      </Card.Group>
-    </div>
-  )
-}
-
-
 
 export default class QuestionHome extends Component {
   render() {
@@ -409,7 +418,6 @@ export default class QuestionHome extends Component {
 
         <p className="question-header">Stats</p>
         <QuestionStats />
-
         <p className="question-header">Random Questions</p>
         <QuestionList {...this.props} />
 
