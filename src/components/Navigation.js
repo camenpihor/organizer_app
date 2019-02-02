b537e0e354b55e3bba8200f1c48ff8f677cc3588
Camen <camenpihor@gmail.com>
2019-02-01 19:32:16 -0500

    Remove unused import

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Icon, Input, Menu, Message, Search, Sidebar } from 'semantic-ui-react'
+import { Input, Menu, Message, Search, Sidebar } from 'semantic-ui-react'
 
 import 'style/navigation.css'
 
